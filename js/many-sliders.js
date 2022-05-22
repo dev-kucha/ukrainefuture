3c10819c78ad771c18c11f22dfd815bae2eb035d
Yurii <ukucher@gmail.com>
2022-05-23 02:55:37 +0300

    проміжковий сейв

--- js/many-sliders.js
+++ js/many-sliders.js
@@ -3,6 +3,16 @@ const aboutwarSlider = document.querySelector('.aboutwar-slider');
 const aboutwarSliderPlate = document.querySelector('.aboutwar-slider_plate');
 const aboutwarSliderStopSlide = document.querySelector('.aboutwar-slider_stop-slide');
 
+const inviSection = document.querySelector('.invi-section');
+const inviSlider = document.querySelector('.invi-section_slider');
+const inviSliderPlate = document.querySelector('.invi-slider_plate ');
+const inviSliderStopSlide = document.querySelector('.invi-slider_stop-slide');
+
+// const aboutHopesSection = document.querySelector('.about-hopes-section');
+// const aboutHopesSlider = document.querySelector('.about-hopes-section_slider');
+// const aboutHopesSliderPlate = document.querySelector('.about-hopes-slider_plate ');
+// const aboutHopesSliderStopSlide = document.querySelector('.about-hopes-slider_stop-slide');
+
 window.addEventListener('scroll', function () {
   if (window.pageYOffset > aboutwarSection.offsetTop) {
     if (
@@ -12,14 +22,6 @@ window.addEventListener('scroll', function () {
       aboutwarSliderPlate.style.left = aboutwarSection.offsetTop - window.pageYOffset + 'px';
     }
   }
-});
-
-const inviSection = document.querySelector('.invi-section');
-const inviSlider = document.querySelector('.invi-section_slider');
-const inviSliderPlate = document.querySelector('.invi-slider_plate ');
-const inviSliderStopSlide = document.querySelector('.invi-slider_stop-slide');
-
-window.addEventListener('scroll', function () {
   if (window.pageYOffset > inviSection.offsetTop) {
     if (
       inviSliderStopSlide.getBoundingClientRect().left > inviSlider.getBoundingClientRect().left
@@ -27,4 +29,12 @@ window.addEventListener('scroll', function () {
       inviSliderPlate.style.left = inviSection.offsetTop - window.pageYOffset + 'px';
     }
   }
+  // if (window.pageYOffset > aboutHopesSection.offsetTop) {
+  //   if (
+  //     aboutHopesSliderStopSlide.getBoundingClientRect().left >
+  //     aboutHopesSlider.getBoundingClientRect().left
+  //   ) {
+  //     aboutHopesSliderPlate.style.left = aboutHopesSection.offsetTop - window.pageYOffset + 'px';
+  //   }
+  // }
 });
